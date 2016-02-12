5f1a7e2da9abc4ca1c3729de0bd26a2ebd6aa368
AsifAmin <asif_udr@yahoo.com>
2016-02-12 13:44:25 +1000

    prac loading

--- glue-module-loader/assets/js/loader.js
+++ glue-module-loader/assets/js/loader.js
@@ -1,20 +1,25 @@
 var dependencyManager = (function(){
     var loadChartdepen =  function(){
-      if($("#charts").length > 0){
-        console.log('chart is present');
-        //head.load("style.css", function(){});
-        head.load(["http://asifamin.github.io/glue-module-lib/assets/js/lib.js", "https://cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.3/highcharts.js"], function() {
-            console.log('highchart lib loaded');
-            });
-        }
+        head.load(["http://asifamin.github.io/glue-module-lib/assets/js/lib.js"], function() {
+            if($("#charts").length > 0){
+                console.log('chart is present');
+
+                //head.load("style.css", function(){});
+                head.load(["https://cdnjs.cloudflare.com/ajax/libs/highcharts/4.2.3/highcharts.js"], function() {
+                    console.log('highchart lib loaded');
+                });
+            }
+        });
     }
 
-  return {
-    loadChartdepen : loadChartdepen()
-  }
+    return {
+        loadChartdepen : loadChartdepen()
+    }
 
 
 })();
 
+
+//# sourceMappingURL=loader.js.map
 
 //# sourceMappingURL=loader.js.map
