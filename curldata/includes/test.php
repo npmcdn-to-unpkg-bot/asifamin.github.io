8b220fdceffaa986b9596ac4147c57743e1a20b7
AsifAmin <asif_udr@yahoo.com>
2016-04-15 16:20:59 +1000

    initial

--- curldata/includes/test.php
+++ curldata/includes/test.php
@@ -1,31 +1,45 @@
 <?php
-//rest api call using curl
+$query = $_GET['sql'];
+$template = $_GET['template'];
 
-$url="https://data.qld.gov.au/api/action/datastore_search_sql?sql=SELECT%20*%20from%20%22384429ae-fd27-4448-afe6-e4ecb8d1ad93%22%20WHERE%20service%20ILIKE%20%27%pay%%27";
+function getDataCr($query, $template)
+{
+    $encodedQuery = urlencode($query);
+    $proxy = '167.123.1.2:8008';
+    $templatePath = "templates/".$template;
+    $url = "https://data.qld.gov.au/api/action/datastore_search_sql?sql=".$encodedQuery;
+//    echo $url;
 //  Initiate curl
-$ch = curl_init();
-// Disable SSL verification
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-// Will return the response, if false it print the response
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// Set the url
-curl_setopt($ch, CURLOPT_URL,$url);
-// Execute
-$result=curl_exec($ch);
-// Closing
-curl_close($ch);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_PROXY, $proxy);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    $result = curl_exec($ch);
+    curl_close($ch);
 //$newResult = json_decode($result, true);
 
 //register mustache library
-require '../Mustache/Autoloader.php';
-Mustache_Autoloader::register();
+    require(realpath(__DIR__ . '/../Mustache/Autoloader.php'));
+//    require '../Mustache/Autoloader.php';
+    Mustache_Autoloader::register();
 
-$entry = new Mustache_Engine;
-$entry_template = file_get_contents('entry.mustache');
+    $entry = new Mustache_Engine;
+    $entry_template = file_get_contents($templatePath);
 //$entry_data = file_get_contents('new.json');
-$entry_data = $result;
-?>
+    $entry_data = $result;
+
+//showing data using mustache template
+    echo $entry->render($entry_template, json_decode($entry_data, true));
+}
 
 
-<?php //showing data using mustache template
-echo $entry->render($entry_template, json_decode($entry_data, true)); ?>
+getDataCr($query , $template);
+
+
+
+
+
+
+
+
