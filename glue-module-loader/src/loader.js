32d6982a99a029b8f5c93de2d88bbf9d35bf394b
AsifAmin <asif_udr@yahoo.com>
2016-05-05 11:49:12 +1000

    file changes

--- glue-module-loader/src/loader.js
+++ glue-module-loader/src/loader.js
@@ -1,7 +1,7 @@
 var glue = {
     commonLibs : function (callback) {
         console.log("common lib loaded");
-        $.getScript("http://asifamin.github.io/glue-module-loader/htdocs/assets/js/charts.js" , callback);
+        $.getScript("http://asifamin.github.io/glue-module-loader/htdocs/assets/js/common.js" , callback);
     },
     loadCharts : function (callback) {
         if($("#charts").length > 0){
