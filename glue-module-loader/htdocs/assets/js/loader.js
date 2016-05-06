430aa49bd316ae05f438c9d3f7b90723356d3f01
AsifAmin <asif_udr@yahoo.com>
2016-05-06 11:31:40 +1000

    file changes

--- glue-module-loader/htdocs/assets/js/loader.js
+++ glue-module-loader/htdocs/assets/js/loader.js
@@ -10,11 +10,11 @@ qg.glue.loader = {
         else this.commonLibs(callback);
     },
     commonLibs : function (callback) {
-        console.log("common lib loaded");
+        console.log('%c loader could not find any specific classes so just the common libraries has been loaded ', 'background: #222; color: #bada55');
         $.getScript("http://asifamin.github.io/glue-module-loader/htdocs/assets/js/common.js" , callback);
     },
     loadCharts : function (callback) {
-        console.log("charts lib loaded");
+        console.log('%c charts libraries has been loaded', 'background: #222; color: #bada55');
         $.getScript("http://asifamin.github.io/glue-module-loader/htdocs/assets/js/charts.js" , callback);
     }
 };
