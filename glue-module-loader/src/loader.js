9911f106e1ec6a31114cf41ea203405c7028d9d5
AsifAmin <asif_udr@yahoo.com>
2016-05-10 16:42:30 +1000

    file changes

--- glue-module-loader/src/loader.js
+++ glue-module-loader/src/loader.js
@@ -7,7 +7,6 @@ qg.glue = qg.glue || {};
 
 qg.glue.loader = {
     init : function (callback) {
-        this.checkJquery();
         if($(".qg-glue-charts").length > 0)  this.loadCharts(callback);
         else this.commonLibs(callback);
     },
@@ -18,23 +17,6 @@ qg.glue.loader = {
     loadCharts : function (callback) {
         console.log('%c charts libraries has been loaded', 'background: #222; color: #bada55');
         $.getScript("http://asifamin.github.io/glue-module-loader/htdocs/assets/js/charts.js" , callback);
-    },
-    checkJquery : function () {
-        var jQueryScriptOutputted = false;
-        //if the jQuery object isn't available
-        if (typeof(jQuery) == 'undefined') {
-
-            if (! jQueryScriptOutputted) {
-                //only output the script once..
-                jQueryScriptOutputted = true;
-
-                //output the script (load it from google api)
-                document.write("<scr" + "ipt type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js'></scr" + 'ipt>')
-            }
-
-        } else {
-            console.log("jquery is already there");
-        }
     }
 };
 
